313898afdcf1facd4d9408f8b4eb7bcb0db10789
VianeyVT <vianeyv_34@hotmail.com>
2014-09-20 20:04:43 -0500

    Revert "Revert "PAssignment2""
    This reverts commit d99bb2b5271ce6f91489c3015f5d3975e715d623.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+#MakeCacheMatrix creates a special matrix containing a function to: 
+#set the value of the matrix
+#get the value of the matrix
+#set the inverse of the matrix
+#get the inverse of the marix
 
-## Write a short comment describing this function
+#cacheSolve computes the inverse of the MakeCacheMatrix's result 
+#in case the result has already been calculated or the matrix isn't different
+#the function retrieve the inverse from the cache
+
+
+#makeCacheMatrix has a matrix as argument,sets and gets the values of the matrix
+#and set and get the inverse
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#cacheSolve takes the result of makeCacheMatrix and retrieve the inverse from
+#the cache or calculates it.
+cacheSolve <- function(x) {
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data)
+  x$setsolve(m)
+  m
 }
